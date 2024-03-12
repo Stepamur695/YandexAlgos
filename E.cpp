9ed399c64526522b198df9ac5816d2b8800ddf3d
Stepamur <murashov.ss@phystech.edu>
2024-03-12 15:59:59 +0300

    Modified E.cpp, but it failed on the 14th test

---
+++ E.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int Calc (long long int n, long long int k);
+
+int main ()
+{
+    long long int n, k, d;
+    scanf ("%lld %lld %lld", &n, &k, &d);
+
+    n = Calc(n, k);
+
+    if (n == -1)
+    {
+        printf("%lld", n);
+        return 0;
+    }
+
+    for (long long int j = 1; j < d; j++)
+    {
+        n = n * 10;
+    }
+
+    printf("%lld", n);
+    return 0;
+}
+
+long long int Calc (long long int n, long long int k)
+{
+    for (long long int i = 0; i < 10; i++)
+    {
+        if ((n*10 + i)%k == 0)
+        {
+            return (n*10 + i);
+        }
+    }
+    
+    return -1;
+}
